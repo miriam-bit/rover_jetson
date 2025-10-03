4959c1d75ca59cc89af930e1ef29237fee9e6bc6
Miriam Vitolo <148908141+miriam-bit@users.noreply.github.com>
2025-10-03 15:49:18 +0200

    Update rover.launch.py

--- src/rover_launch/launch/rover.launch.py
+++ src/rover_launch/launch/rover.launch.py
@@ -7,16 +7,32 @@ import os
 
 def generate_launch_description():
     
+    # Percorso al file pix_rover_driver.launch.xml
     pix_rover_launch = os.path.join(
         get_package_share_directory('pix_rover_driver'),
         'launch',
         'pix_rover_driver.launch.xml'
     )
+
     # Percorso al file socket_can_bridge.launch.xml
     socket_can_bridge_launch = os.path.join(
         get_package_share_directory('ros2_socketcan'),
         'launch',
         'socket_can_bridge.launch.xml'
+    )
+
+    # Percorso al file active_node.launch.py del pacchetto dwm1001_launch
+    dwm1001_launch = os.path.join(
+        get_package_share_directory('dwm1001_launch'),
+        'launch',
+        'active_node.launch.py'
+    )
+
+    #  Percorso al file anchors.launch.py
+    anchors_launch = os.path.join(
+        get_package_share_directory('rover_launch'),
+        'launch',
+        'anchors.launch.py'
     )
 
     return LaunchDescription([
@@ -28,13 +44,23 @@ def generate_launch_description():
             output='screen'
         ),
 
-        # Include file XML usando AnyLaunchDescriptionSource
+        # Include file socket_can_bridge.launch.xml
         IncludeLaunchDescription(
             AnyLaunchDescriptionSource(socket_can_bridge_launch)
         ),
-        
+
+        # Include file pix_rover_driver.launch.xml
         IncludeLaunchDescription(
             AnyLaunchDescriptionSource(pix_rover_launch)
         ),
-    ])
 
+        # Include file active_node.launch.py
+        IncludeLaunchDescription(
+            AnyLaunchDescriptionSource(dwm1001_launch)	
+        ),
+
+        # Include file anchors.launch.py
+        IncludeLaunchDescription(
+            AnyLaunchDescriptionSource(anchors_launch)
+        ),
+    ])
